11dfed5eb0788557ea2b51f5e16d775a97b57026
Akash <25547159+sirakashaman@users.noreply.github.com>
2021-06-01 08:23:12 +0530

    update

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Make sure all the pages are loaded before you run the script from browser console.
-var images = Array.from(document.getElementsByTagName('main')[0].getElementsByTagName('img'));
+//var images = Array.from(document.getElementsByTagName('main')[0].getElementsByTagName('img'));
+var images = Array.from(document.getElementsByClassName("jss516"));
 console.log(images);
 images.forEach(eachImg => {
     if (eachImg.alt.startsWith('page')) {
